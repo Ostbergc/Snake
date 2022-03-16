7e738e4be672b4fcf90bcce15fe01a455cd11728
Ostbergc <chrilleostberg@hotmail.com>
2022-03-16 09:36:23 +0100

    Updates to main.js
    Added variables to represent the snakes grid and pixels (size 20 == 400 (canvas size).
    
    Snakes x & y coordinates generated randomly with Math.random function.
    
    Array to represent snake and default snake length.
    
    A function to color the background and snake called from within gameLoop.
    
    Then lastly a push of x & y to the array to create the snake

--- main.js
+++ main.js
@@ -6,6 +6,40 @@ window.onload = function () {
   //call gameLoop every 1000ms
   setInterval(gameLoop, 1000);
 }
+
+//Grid and pixel size
+var grid = 20;
+var pixel = 20;
+
+//Snake position
+var snakeX = Math.floor(Math.random() * pixel);
+var snakeY = Math.floor(Math.random() * pixel);
+
+//Array to represent the snake
+var snake = [];
+
+//Default snake length = 3
+var snakeLength = 3;
+
 function gameLoop() {
 
+  //Call colorBackground function to fill canvas background
+  colorBackground();
+
+  //Call colorSnake function to color the snake
+  colorSnake();
+
+  //Push x & y into the snake array to make the snake
+  snake.push({ x: snakeX, y: snakeY });
+
+}
+function colorSnake() {
+  ctx.fillStyle = "white";
+  for (var i = 0; i < snake.length; i++) {
+    ctx.fillRect(snake[i].x * grid, snake[i].y * grid, grid, grid);
+  }
+}
+function colorBackground() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canv.width, canv.height);
 }
